25effcbfff91956ba04ee2b7351ec25447d69c37
Nicolas Alpi <nicolas.alpi@gmail.com>
2012-09-08 08:30:52 +0100

    Fix error on non backorder allowed

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,5 +1,5 @@
 Spree::LineItem.class_eval do
-  
+
   validate :validate_quantity_and_stock
 
   private
@@ -11,13 +11,13 @@ Spree::LineItem.class_eval do
     # avoid reload of order.inventory_units by using direct lookup
     unless !Spree::Config[:track_inventory_levels]                        ||
            Spree::Config[:allow_backorders]                               ||
-           order   && Spree::InventoryUnit.order_id_equals(order).first.present? ||
+            order   && Spree::InventoryUnit.where(:order_id => order.id).first.present? ||
            variant && quantity <= variant.on_hand
       errors.add(:quantity, I18n.t("validation.is_too_large") + " (#{self.variant.name})")
     end
 
     return unless variant
-    
+
 =begin # Commented out until Order#shipped_units not restored in the Core
     if variant.product.assembly?
       variant.product.parts.each do |part|
@@ -31,7 +31,7 @@ Spree::LineItem.class_eval do
       end
     end
 =end
-    
+
   end
 
 end
