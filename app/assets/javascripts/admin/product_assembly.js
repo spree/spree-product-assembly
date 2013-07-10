badb4b4850ba2b64890dc2ec8ae1518bb0b66a93
Tyler Mercier <tylermercier@gmail.com>
2013-07-10 13:13:57 -0600

    extract JS into module

--- app/assets/javascripts/admin/product_assembly.js
+++ app/assets/javascripts/admin/product_assembly.js
@@ -1,29 +1,31 @@
- $(function() {
+window.SP = SP || {};
+
+$(function() {
   $("#searchtext").keypress(function (e) {
     if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-      search_for_parts();
+      window.SP.search_for_parts();
       return false;
     } else {
-     return true;
-   }
- });
+      return true;
+    }
+  });
 
   $("#searchtext").delayedObserver(function(element, value) {
-    search_for_parts();
+    window.SP.search_for_parts();
   }, 0.75);
 
-  function subscribe_product_part_links() {
+  window.SP.subscribe_product_part_links = function() {
     $("a.set_count_admin_product_part_link").click(function(){
       params = { count :  $("input", $(this).parent().parent()).val() };
-      return make_post_request($(this), params);
+      return window.SP.make_post_request($(this), params);
     });
 
     $("a.remove_admin_product_part_link").click(function(){
-      return make_post_request($(this), {});
+      return window.SP.make_post_request($(this), {});
     });
-  }
+  };
 
-  function make_post_request(link, post_params) {
+  window.SP.make_post_request = function(link, post_params) {
     spinner = $("img.spinner", link.parent());
     spinner.show();
     $.post(link.attr("href"), post_params,
@@ -31,7 +33,7 @@
       "script");
 
     return false;
-  }
+  };
 
-  subscribe_product_part_links();
+  window.SP.subscribe_product_part_links();
 });
