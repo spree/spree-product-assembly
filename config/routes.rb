4c630d25ea862123477ddeba9bfaa5a78e4dc70d
Steven Barragan <me@steven.mx>
2014-03-07 18:07:28 -0600

    Change parts route to work with variants

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Spree::Core::Engine.routes.append do
 
   namespace :admin do
     resources :products do
-      resources :parts do
-        member do
-          post :select
-          post :remove
-          post :set_count
-        end
-        collection do
-          post :available
-          get  :selected
+      resources :variants do
+        resources :parts do
+          member do
+            post :select
+            post :remove
+            post :set_count
+          end
+          collection do
+            post :available
+            get  :selected
+          end
         end
       end
     end
