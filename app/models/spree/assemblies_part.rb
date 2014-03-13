2879b761b8344425a5df9b0fd9c34daa792a2471
Steven Barragan <me@steven.mx>
2014-03-12 19:08:08 -0600

    Fix relation in belongs_to :assembly in AssembliesPart

--- app/models/spree/assemblies_part.rb
+++ app/models/spree/assemblies_part.rb
@@ -1,6 +1,6 @@
 module Spree
   class AssembliesPart < ActiveRecord::Base
-    belongs_to :assembly, :class_name => "Spree::Product", :foreign_key => "assembly_id"
+    belongs_to :assembly, :class_name => "Spree::Variant", :foreign_key => "assembly_id"
     belongs_to :part, :class_name => "Spree::Variant", :foreign_key => "part_id"
 
     def self.get(assembly_id, part_id)
