85fe1d321505f5466c6fe8b1f79305951c3cb1be
mike <mstate@gmail.com>
2015-02-12 17:03:02 -0500

    Validate only the quantity of parts necessary to meet additions to the line_item's quantity (instead of the entire quantity).

--- app/models/spree/stock/availability_validator.rb
+++ app/models/spree/stock/availability_validator.rb
@@ -3,14 +3,17 @@ module Spree
     # Overridden from spree core to make it also check for assembly parts stock
     class AvailabilityValidator < ActiveModel::Validator
       def validate(line_item)
+        unit_count = line_item.inventory_units.size  
+        return if unit_count >= line_item.quantity
+        
         product = line_item.product
 
         valid = if product.assembly?
           line_item.parts.all? do |part|
-            Stock::Quantifier.new(part).can_supply?(line_item.count_of(part) * line_item.quantity)
+            Stock::Quantifier.new(part).can_supply?(line_item.count_of(part) * (line_item.quantity - unit_count))
           end
         else
-          Stock::Quantifier.new(line_item.variant).can_supply? line_item.quantity
+          Stock::Quantifier.new(line_item.variant).can_supply?(line_item.quantity - unit_count)
         end
 
         unless valid
