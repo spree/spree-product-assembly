c3befb14f0338c51bc248f64459c0d7b601bf699
Cindy Ward <cindy@trialguides.com>
2015-03-03 19:02:24 -0800

    Implement inventory tracking using parts based on master variant track_inventory flag value

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -10,4 +10,20 @@ Spree::Variant.class_eval do
   def part?
     assemblies.exists?
   end
+
+  def sku_and_options_text(show_product_name=false)
+    if !show_product_name
+      "#{sku} #{options_text}".strip
+    else
+      "#{name}: #{options_text}".strip
+    end
+  end
+
+  # Shortcut method to determine if inventory tracking is enabled for this variant
+  # This considers both variant tracking flag and site-wide inventory tracking settings
+  # Add checking for assembly with master variant that doesn't track inventory
+  def should_track_inventory?
+    self.track_inventory? && Spree::Config.track_inventory_levels || self.product.assembly? && self.is_master?
+  end
+
 end
