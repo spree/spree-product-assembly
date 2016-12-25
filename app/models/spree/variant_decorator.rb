6c5a69b53c887e4fcf0a9dd051401102e1ad37cc
Gurpreet Singh <gurpreet@vinsol.com>
2016-12-25 14:26:38 +0530

    ensure no inventory unit present before destroy any variant

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -5,6 +5,8 @@ Spree::Variant.class_eval do
   has_many :assemblies, through: :assemblies_variants, class_name: "Spree::Variant", dependent: :destroy
   has_many :parts, through: :parts_variants, class_name: "Spree::Variant", dependent: :destroy
 
+  before_destroy :ensure_no_inventory_units
+
   def assemblies_for(products)
     assemblies.where(id: products)
   end
@@ -12,4 +14,12 @@ Spree::Variant.class_eval do
   def part?
     assemblies.exists?
   end
+
+  private
+    def ensure_no_inventory_units
+      if inventory_units.any?
+        errors.add(:base, Spree.t(:cannot_destroy_if_attached_to_inventory_units))
+        return false
+      end
+    end
 end
