26eb731677a72fb1bac1d7e2828aa6ecb22e3b40
Gurpreet Singh <gurpreet@vinsol.com>
2016-12-25 15:22:03 +0530

    ruby optimization - map not required

--- app/models/spree/inventory_unit_decorator.rb
+++ app/models/spree/inventory_unit_decorator.rb
@@ -3,7 +3,7 @@ module Spree
     def percentage_of_line_item
       product = line_item.product
       if product.assembly?
-        total_value = line_item.quantity_by_variant.map { |part, quantity| part.price * quantity }.sum
+        total_value = line_item.quantity_by_variant.sum { |part, quantity| part.price * quantity }
         variant.price / total_value
       else
         1 / BigDecimal.new(line_item.quantity)
