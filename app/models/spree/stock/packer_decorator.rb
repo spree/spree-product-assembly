cde6a8a6628206507e80fab6b9f3da0baa2234b5
KD <kate_lani@wp.pl>
2017-04-06 11:32:40 +0200

    refactor package_decorator.rb

--- app/models/spree/stock/packer_decorator.rb
+++ app/models/spree/stock/packer_decorator.rb
@@ -6,14 +6,13 @@ module Spree
         package = Package.new(stock_location)
 
         inventory_units.each do |inventory_unit|
-
           variant = inventory_unit.variant
           unit = inventory_unit.dup # Can be used by others, do not use directly
           if variant.should_track_inventory?
             next unless stock_location.stocks? variant
             on_hand, backordered = stock_location.fill_status(variant, unit.quantity)
-            package.add(InventoryUnit.split(unit, backordered), :backordered) if backordered.positive?
-            package.add(InventoryUnit.split(unit, on_hand), :on_hand) if on_hand.positive?
+            package.add(unit, :backordered) if backordered.positive?
+            package.add(unit, :on_hand) if on_hand.positive?
           else
             package.add unit
           end
