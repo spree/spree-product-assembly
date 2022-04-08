e8c5691f7babf970681c304b1c77ee2aa7f77608
bluebash-dev <developer@bluebash.co>
2022-04-08 19:05:59 +0530

    bug fix for stock manage

--- app/controllers/spree/admin/parts_controller.rb
+++ app/controllers/spree/admin/parts_controller.rb
@@ -1,18 +1,19 @@
 class Spree::Admin::PartsController < Spree::Admin::BaseController
   helper_method :product
+  before_action :find_assembly_part, only: [:set_count, :remove]
 
   def index
     @parts = product.assemblies_parts.includes(:assembly, :part)
   end
 
   def remove
-    assembly_part = Spree::AssembliesPart.find(params[:id])
-    assembly_part.destroy
+    @assembly_part.delete_all if assembly_part.present?
     render 'spree/admin/parts/update_parts_table'
   end
 
   def set_count
-    save_part(existing_part_params)
+    @assembly_part.update_all(count: params[:count], part_id: params[:change_part_id]) if @assembly_part.present?
+    render 'spree/admin/parts/update_parts_table'
   end
 
   def available
@@ -34,30 +35,34 @@ class Spree::Admin::PartsController < Spree::Admin::BaseController
 
   private
 
-    def save_part(part_params)
-      form = Spree::AssignPartToBundleForm.new(product, part_params)
-      if form.submit
-        render 'spree/admin/parts/update_parts_table'
-      else
-        error_message = form.errors.full_messages.to_sentence
-        render json: error_message.to_json, status: 422
-      end
+  def save_part(part_params)
+    form = Spree::AssignPartToBundleForm.new(product, part_params)
+    if form.submit
+      render 'spree/admin/parts/update_parts_table'
+    else
+      error_message = form.errors.full_messages.to_sentence
+      render json: error_message.to_json, status: 422
     end
+  end
 
-    def product
-      @product ||= Spree::Product.find_by(slug: params[:product_id])
-    end
+  def product
+    @product ||= Spree::Product.find_by(slug: params[:product_id])
+  end
 
-    def new_part_params
-      params.require(:assemblies_part).permit(
-        :count,
-        :part_id,
-        :assembly_id,
-        :variant_selection_deferred
-      )
-    end
+  def new_part_params
+    params.require(:assemblies_part).permit(
+      :count,
+      :part_id,
+      :assembly_id,
+      :variant_selection_deferred
+    )
+  end
 
-    def existing_part_params
-      params.permit(:id, :count)
-    end
+  def existing_part_params
+    params.permit(:id, :count, :part_id)
+  end
+
+  def find_assembly_part
+    @assembly_part = Spree::AssembliesPart.where(assembly_id: params[:id], part_id: params[:part_id])
+  end
 end
